b6fa9843d28b60eae9ea73caea84032031189b40
Brewqser <michal.czwornog124812@gmail.com>
2018-08-12 18:39:09 +0200

    kinga works

--- main.cpp
+++ main.cpp
@@ -10,26 +10,55 @@ using namespace std;
 struct point {int i,j;};
 
 vector <point> V;
+int grid[100][100]={0};
+int sgrid[100][100]={0};
 int w=10;
-int h=10;
+int h=9;
 int pixel=32;
 int bombs=20;
+int marked=0;
+int tilesLeft=0;
 
-int main()
+
+//////Reviling tiles ///////
+void DFS(int x, int y)
 {
-    srand(time(0));
+    if (sgrid[x][y]==10)
+    {
+        sgrid[x][y]=grid[x][y];
+        tilesLeft--;
+        if (sgrid[x][y]==0)
+        {
+            if (sgrid[x+1][y]!=11) DFS(x+1,y);
+            if (sgrid[x-1][y]!=11) DFS(x-1,y);
+            if (sgrid[x][y+1]!=11) DFS(x,y+1);
+            if (sgrid[x][y-1]!=11) DFS(x,y-1);
+            if (sgrid[x+1][y+1]!=11) DFS(x+1,y+1);
+            if (sgrid[x-1][y+1]!=11) DFS(x-1,y+1);
+            if (sgrid[x+1][y-1]!=11) DFS(x+1,y-1);
+            if (sgrid[x-1][y-1]!=11) DFS(x-1,y-1);
+        }
+    }
+}
 
-    RenderWindow app(VideoMode(pixel*(h+2), pixel*(w+2)), "Minesweeper");
-
-    int grid[h+2][w+2]={0};
-    int sgrid[h+2][w+2]={0};
-    Texture t;
-    t.loadFromFile("images/tiles.png");
-    Sprite s(t);
-
-    /////settings//////
+//////Settings//////
+void Settings()
+{
+    tilesLeft=h*w;
     point p;
     for (int i=1;i<=h;i++) for (int j=1;j<=w;j++) sgrid[i][j]=10;
+    for (int i=0;i<=h+1;i++)
+    {
+
+        sgrid[0][i]=11;
+        sgrid[h+1][i]=11;
+    }
+    for (int i=0;i<=w+1;i++)
+    {
+
+        sgrid[i][0]=11;
+        sgrid[i][w+1]=11;
+    }
     for (int i=1;i<=h;i++)
     {
         for (int j=1;j<=w;j++)
@@ -59,37 +88,80 @@ int main()
             }
         }
     }
+}
 
-
-    ////// Consol chackout ////////
-    for (int i=1;i<=h;i++)
+//////Consol chackout ////////
+void Debug()
+{
+    for (int i=0;i<=h+1;i++)
     {
-        for (int j=1;j<=w;j++)
+        for (int j=0;j<=w+1;j++)
         {
             cout << sgrid[i][j] << " ";
         }
         cout << endl;
     }
 
-    for (int i=1;i<=h;i++)
+    for (int i=0;i<=h+1;i++)
     {
-        for (int j=1;j<=w;j++)
+        for (int j=0;j<=w+1;j++)
         {
             cout << grid[i][j] << " ";
         }
         cout << endl;
     }
+}
 
+
+int main()
+{
+    srand(time(0));
+
+    RenderWindow app(VideoMode(pixel*(w+2), pixel*(h+2)), "Minesweeper");
+    Texture t;
+    t.loadFromFile("images/tiles.png");
+    Sprite s(t);
+
+    Settings();
+    Debug();
 
     ////// Main Loop /////
     while (app.isOpen())
     {
+        int tmpy;
+        int tmpx;
         Event e;
         while (app.pollEvent(e))
         {
             if (e.type == Event::Closed) app.close();
-        }
 
+            if (e.type == Event::MouseButtonReleased)
+            {
+                tmpy=e.mouseButton.x/pixel;
+                tmpx=e.mouseButton.y/pixel;
+                //cout << tmpx << " " << tmpy << endl;
+                if (e.mouseButton.button == Mouse::Left)
+                {
+                    //sgrid[tmpx][tmpy]=grid[tmpx][tmpy];
+                    DFS(tmpx,tmpy);
+                }
+                if (e.mouseButton.button == Mouse::Right)
+                {
+                    if (sgrid[tmpx][tmpy]==10)
+                    {
+                        sgrid[tmpx][tmpy]=11;
+                        marked++;
+                    }
+                    else if (sgrid[tmpx][tmpy]==11)
+                    {
+                        sgrid[tmpx][tmpy]=10;
+                        marked--;
+                    }
+                }
+                //cout << "marked == " << marked << endl;
+            }
+
+        }
         app.clear(Color::White);
 
         for (int i=1;i<=h;i++)
@@ -97,7 +169,35 @@ int main()
             for (int j=1;j<=w;j++)
             {
                 s.setTextureRect(IntRect(sgrid[i][j]*pixel,0,pixel,pixel));
-                s.setPosition(i*pixel,j*pixel);
+                s.setPosition(j*pixel,i*pixel);
+                app.draw(s);
+            }
+        }
+
+        if (Mouse::isButtonPressed(Mouse::Left))
+        {
+            Vector2i localPosition = Mouse::getPosition(app);
+            s.setTextureRect(IntRect(0,0,pixel,pixel));
+            tmpx=localPosition.x/pixel;
+            tmpy=localPosition.y/pixel;
+            cout << tmpx << " " << tmpy << endl;
+            if (tmpx <= h && tmpx >=1 && tmpy >=1 && tmpy <=w && sgrid[tmpy][tmpx]==10)
+            {
+                s.setPosition(tmpx*pixel,tmpy*pixel);
+                app.draw(s);
+            }
+        }
+
+        if (Mouse::isButtonPressed(Mouse::Right))
+        {
+            Vector2i localPosition = Mouse::getPosition(app);
+            s.setTextureRect(IntRect(11*32,0,pixel,pixel));
+            tmpx=localPosition.x/pixel;
+            tmpy=localPosition.y/pixel;
+            cout << tmpx << " " << tmpy << endl;
+            if (tmpx <= h && tmpx >=1 && tmpy >=1 && tmpy <=w && sgrid[tmpy][tmpx]==10)
+            {
+                s.setPosition(tmpx*pixel,tmpy*pixel);
                 app.draw(s);
             }
         }
