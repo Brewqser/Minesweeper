821462fb886c14f1842fccd4df8a333c0e339903
Brewqser <michal.czwornog124812@gmail.com>
2018-08-12 15:08:02 +0200

    Main changes

--- main.cpp
+++ main.cpp
@@ -4,21 +4,52 @@
 using namespace sf;
 using namespace std;
 
+int w=10;
+int h=10;
+int pixel=32;
+
+
 int main()
 {
-    RenderWindow window(VideoMode(320, 320), "Minesweeper");
+    RenderWindow app(VideoMode(pixel*(h+2), pixel*(w+2)), "Minesweeper");
 
-    while (window.isOpen())
+    int grid[h+2][w+2];
+    int sgrid[h+2][w+2];
+    Texture t;
+    t.loadFromFile("images/tiles.png");
+    Sprite s(t);
+
+    for (int i=1;i<=h;i++) for (int j=1;j<=w;j++) sgrid[i][j]=10;
+    for (int i=1;i<=h;i++)
+    {
+        for (int j=1;j<=w;j++)
+        {
+            cout << sgrid[i][j] << " ";
+        }
+        cout << endl;
+    }
+    while (app.isOpen())
     {
         Event e;
-        while (window.pollEvent(e))
+        while (app.pollEvent(e))
         {
-            if (e.type == Event::Closed)
-                window.close();
+            if (e.type == Event::Closed) app.close();
         }
 
-        window.clear();
-        window.display();
+        app.clear(Color::White);
+
+
+        for (int i=1;i<=h;i++)
+        {
+            for (int j=1;j<=w;j++)
+            {
+                s.setTextureRect(IntRect(sgrid[i][j]*pixel,0,pixel,pixel));
+                s.setPosition(i*pixel,j*pixel);
+                app.draw(s);
+            }
+        }
+
+        app.display();
     }
     return 0;
 }
